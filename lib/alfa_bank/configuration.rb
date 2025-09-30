9b439fe174820cb8e0258b2bc284d1a7a311e87a
Evgeniy Shelest <eshelest@onlinetours.ru>
2025-09-30 18:38:12 +0300

    Add language to gem configuration.

--- lib/alfa_bank/configuration.rb
+++ lib/alfa_bank/configuration.rb
@@ -1,11 +1,12 @@
 module AlfaBank
   class Configuration
-    attr_accessor :user_name, :password, :base_link
+    attr_accessor :user_name, :password, :base_link, :language
 
     def initialize
       @user_name = "user_name"
       @password = "password"
       @base_link = "http://alfa_bank_test_url.ru"
+      @language = "en"
     end
   end
 end
